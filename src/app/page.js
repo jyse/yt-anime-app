af7d9ba49ca3b49d4d8d5fd4de0f7f920516cc98
jyse <jessythe@gmail.com>
2024-03-25 01:54:52 +0100

    changes right padding and simplifying divs and css styles

--- src/app/page.js
+++ src/app/page.js
@@ -86,84 +86,78 @@ function App() {
       <div className="container">
         <Header />
         <div className="content">
-          <div className="media">
+          <div className="media-section">
             <div className="media-header">
-              <div className="left">
+              <div className="media-title">
                 <img src="/images/naruto.png" alt="naruto" />
                 <h2>UPCOMING</h2>
               </div>
-              <div className="right">
-                <h2>1</h2>
-                <h2>2</h2>
-                <h2>3</h2>
-                <h2>4</h2>
-                <h2>5</h2>
-              </div>
-            </div>
-            <div className="episodes-upcoming">
-              <div className="episodes-upcoming-grid">
-                {upcoming?.map((upcomingEp) => {
-                  return (
-                    <UpcomingCard
-                      episode={upcomingEp}
-                      key={upcomingEp.mal_id}
-                    />
-                  );
+              <div className="pagination">
+                {[1, 2, 3, 4, 5].map((num) => {
+                  return <h2 key={num}>{num}</h2>;
                 })}
               </div>
             </div>
-            <div className="most-popular-tiles">
-              <div className="media-header">
+            <div className="episodes-grid upcoming">
+              {upcoming?.map((upcomingEp) => {
+                return (
+                  <UpcomingCard episode={upcomingEp} key={upcomingEp.mal_id} />
+                );
+              })}
+            </div>
+
+            <div className="media-header">
+              <div className="media-title">
                 <img src="/images/sasuke.png" alt="sasuke" />
                 <h2> CURRENTLY AIRING</h2>
               </div>
-              {popularAnime.length > 0 ? (
-                <div className="episodes-popular-grid">
-                  {popularAnime?.map((popularAnime) => (
-                    <AiringCard
-                      episode={popularAnime}
-                      key={popularAnime.mal_id}
-                    />
-                  ))}
-                </div>
-              ) : (
-                <p>Loading airing anime</p>
-              )}
             </div>
+            {popularAnime.length > 0 ? (
+              <div className="episodes-grid airing">
+                {popularAnime?.map((popularAnime) => (
+                  <AiringCard
+                    episode={popularAnime}
+                    key={popularAnime.mal_id}
+                  />
+                ))}
+              </div>
+            ) : (
+              <p>Loading airing anime</p>
+            )}
           </div>
           <div className="lists-section">
-            <div className="lists-advertisements">
-              <div className="media-header">
+            <div className="media-header">
+              <div className="media-title">
                 <img src="/images/kiba.png" alt="kiba" />
                 <h2> ADVERTISEMENTS</h2>
               </div>
-              <div className="advertisement">
-                <img src="/images/advertisement.png" alt="kimetsu" />
-              </div>
             </div>
-            <div className="lists-top">
-              <div className="media-header">
+            <div className="advertisement">
+              <img src="/images/advertisement.png" alt="kimetsu" />
+            </div>
+
+            <div className="media-header">
+              <div className="media-title">
                 <img src="/images/sakura.png" alt="kiba" />
                 <h2> POPULAR </h2>
               </div>
-              <div className="top-shows">
-                <div className="top-shows-list">
-                  {popular?.map((popEp) => {
-                    return <p>{popEp.newTitle}</p>;
-                  })}
-                </div>
-              </div>
             </div>
-            <div className="lists-genre">
-              <div className="media-header">
+            <div className="popular-shows">
+              {popular?.map((popEp) => {
+                return <p>{popEp.newTitle}</p>;
+              })}
+            </div>
+
+            <div className="media-header">
+              <div className="media-title">
                 <img src="/images/neji.png" alt="neji" />
                 <h2> GENRES</h2>
               </div>
-              <div className="genresList">
-                {genresList?.map((genre) => {
-                  return <p>{genre}</p>;
-                })}
-              </div>
+            </div>
+            <div className="genres-anime">
+              {genresList?.map((genre) => {
+                return <p>{genre}</p>;
+              })}
             </div>
           </div>
         </div>
